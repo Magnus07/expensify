efba5604f699007d6b2fd8716e3e680b9581cb9c
Magnus <magnus@MacBook-Pro-2.local>
2024-01-22 21:39:07 +0200

    init - project init

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+interface Props {}
+
+const App = (props: Props) =>{
+
+  return (<><Text className="bg-red-700">Hello</Text></>)
+}
+
+export default App
